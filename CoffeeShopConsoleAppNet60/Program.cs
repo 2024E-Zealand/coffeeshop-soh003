08510106a6b61fd75b7ac0b210a8acd8f32d5461
soh003 <149380282+soh003@users.noreply.github.com>
2024-09-02 14:51:12 +0200

    der er oprettet instancer af klasserne samt udskrevet pris for black coffee

--- CoffeeShopConsoleAppNet60/Program.cs
+++ CoffeeShopConsoleAppNet60/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CoffeeShopConsoleAppNet60;
+
 Console.WriteLine("Hello, World!");
+
+BlackCoffee blackCoffee = new BlackCoffee();
+Console.WriteLine($"Sort Kaffe : {blackCoffee.Price()} DKK" );
+
+Latte latte = new Latte();
+
+Cortado cortado = new Cortado();    
